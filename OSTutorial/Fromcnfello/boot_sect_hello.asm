02b791d86025a388394cc244914f312853d34c2a
enigmatized <54577107+enigmatized@users.noreply.github.com>
2019-09-27 18:34:16 -0700

    Just uploading bootloader Tutorial1

---
+++ OSTutorial/Fromcnfello/boot_sect_hello.asm
@@ -0,0 +1,33 @@
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; 'l' is still on al, remember?
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+
+mov al, 'L'
+int 0x10
+mov al, 'i'
+int 0x10
+mov al, 'v'
+int 0x10
+mov al, 'i'
+int 0x10
+mov al, 'a'
+int 0x10
+
+
+
+
+
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55 
